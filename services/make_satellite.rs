21818f7a54e5a8742db3b97a07b5c474aa8f7ae4
WalterSharp <walter.sharp@overlander.co.za>
2018-09-21 17:34:17 +0200

    Added cargo config file to project Added make_satellite and slack service files to project Added initiateGoodVibes method to make_satellite module

---
+++ services/make_satellite.rs
@@ -0,0 +1,18 @@
+extern crate reqwest;
+use reqwest;
+
+/**Attempts to initialise the good vibes satellite via a call to the Make satellite REST endpoint
+ * username: The username used to authenticate with the satellite server
+ * password: The password used to authenticate with the satellite server
+ */
+fn initialiseGoodVibes(username: String, password: String)
+{
+	let client = Client::new();
+	let credentials = header::Basic { username: username, password: password };
+
+	let request = client.post("https://make.offerzen.com/satellite/");
+	request.header(header::Authorization(credentials));
+	request.body("");
+	
+	let response = request.send();
+}
